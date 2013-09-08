3c5418c524f0427f575bf93ee9c8a303c2065f80
webdude <zorinj@gmail.com>
2013-09-08 23:39:42 +0400

    example const static intwerfaces abstract

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 ﻿<?
-class User {
+abstract class Person {
+	const JUSTUSER = 1;
+	const SHOPPER = 2;
+
 	protected $logged = false;
 	protected $username = "";
+	protected $level;
 
-	public function __construct($username) {
+	public function __construct($username, $level) {
 		$this->username = $username;
+		$this->level = $level;
+	}
+
+	public function getlevel() {
+		switch ($this->level) {
+			case self::JUSTUSER:
+				return "Обычный пользователь";
+				break;
+			
+			default:
+				return "Вы не авторизованы";
+				break;
+			case self::SHOPPER:
+				return "Покупатель";
+		}
 	}
 
 	public function __toString() {
@@ -19,39 +38,113 @@ class User {
 		$this->logged = false;
 	}
 
-	private function isLogged() {
+	protected function isLogged() {
 		return $this->logged;
 	}
 
 	public function getUsername() {
-		return $this->username."<br>";
+		return $this->username;
+	}
+
+	abstract public function showMessage();
+}
+
+class User extends Person implements Persistable {
+	public function save() {
+		echo $this->getUsername()." was saved to database";
+	}
+
+	public function load() {
+		echo $this->getUsername()." is load from database";
+	}
+
+	public function delete() {
+		echo $this->getUsername()." is delete from database";
 	}
 
 	public function showMessage() {
-		echo ($this->isLogged() ? $this->username." залогинился<br>":
-		$this->username." разалогинился<br>");
+		echo "Welcome, ".$this->getUsername();
 	}
-
 }
 
-class Admin extends User{
-	
-	public function getUsername() {
-		parent::getUsername();
-		return $this->username." администратор";
+class Topic implements Persistable {
+	private $subject = "";
+	private $username = "";
+	private $authorname = "";
+	private static $numobject = 0;
+
+	public function __construct($subject, $username) {
+		$this->subject = $subject;
+		$this->username = $username;
+		$this->authorname = $this->username->getUsername();
+		self::$numobject++;
 	}
 
+	public static function getNumObject() {
+		return self::$numobject;
+	}
+
+	public function save() {		
+		echo $this->authorname." add new topic";
+	}
+
+	public function load() {
+		echo $this->authorname." load $this->subject topic";
+	}
+
+	public function delete() {
+		echo $this->authorname." delete $this->subject database";
+	}	
+}
+class Shopper extends Person implements Persistable {
+
+	public function save() {
+		echo "<br>Your order was saved to database<br>";
+	}
+
+	public function load() {
+		echo "<br>Your order is load from database<br>";
+	}
+
+	public function delete() {
+		echo "<br>Your order is delete from database<br>";
+	}
+
+	public function showMessage() {
+		echo "Welcome to online shop, ".$this->getUsername();
+	}
+
+	public function addToCart($item) {
+		echo "$item adding in your cart";
+	}
 }
 
-$objUser = new User("Web");
-echo $objUser->getUsername();
-$objUser->login();
-$objUser->showMessage();
-$objUser->logout();
-$objUser->showMessage();
-echo $objUser;
-echo "<br>---------------<br>";
+interface Persistable {
+	public function save();
+	public function load();
+	public function delete();
+}
 
-$objAdmin = new Admin("Webdude");
-echo $objAdmin->getUsername();
+$objUser = new User("Web", 1);
+echo $objUser->getlevel();
+echo "<br>";
+$objUser->showMessage();
+echo "<br>";
+$objUser->delete();
+echo "<br>";
+$Topic = new Topic("New theme", $objUser);
+$Topic->load();
+
+echo "<br>";
+$Topic2 = new Topic("New theme2", $objUser);
+echo "Создано ".Topic::getNumObject()."  топика<br>";
+$Topic2->load();
+echo "<br>------<br>";
+$objShopper = new Shopper("Webdude", 2);
+echo $objShopper->getlevel();
+echo "<br>";
+$objShopper->showMessage();
+echo "<br>";
+$objShopper->addToCart(" Ployer Momo 7");
+$objShopper->save();
 ?>
